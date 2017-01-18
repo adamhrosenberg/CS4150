a06b5aa837d1e62ed92eda35dea16aa81adb0469
Adam Rosenberg <adam.arosenberg@gmail.com>
2017-01-18 15:53:29 -0700

    Update shortestpath.cpp

--- shortestpath.cpp
+++ shortestpath.cpp
@@ -12,25 +12,7 @@
 using namespace std;
 
 /**
-
-
-
-Input
-The input consists of several test cases. Each test case starts with a line with four non-negative integers, 
-1≤n≤100001≤n≤10000, 0≤m≤300000≤m≤30000, 1≤q≤1001≤q≤100 and 0≤s<n0≤s<n, separated by single spaces, where nn is the numbers of nodes in the graph, 
-mm the number of edges, qq the number of queries and ss the index of the starting node. Nodes are numbered from 00 to n−1n−1. 
-Then follow mm lines, each line consisting of three (space-separated) integers uu, vv and ww indicating that there is an edge 
-from uu to vv in the graph with weight 0≤w≤10000≤w≤1000. Then follow qq lines of queries, each consisting of a single non-negative integer, 
-asking for the minimum distance from node ss to the node number given on the query line.
-
-Input will be terminated by a line containing four zeros, this line should not be processed.
-
-Output
-For each query, output a single line containing the minimum distance from node ss to the node specified in the query, or the word “Impossible” 
-if there is no path from ss to that node. Print a blank line after each test case.
-
-
-
+Given a weighted directional cyclic graph, find the cheapest path from a to b, otherwise report impossible. 
 **/
 struct path{
     int source;
