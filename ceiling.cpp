67d370caf2b30dab17077ea60c191f3ac65f2be4
Adam Rosenberg <adam.arosenberg@gmail.com>
2017-01-18 15:40:45 -0700

    Update ceiling.cpp

--- ceiling.cpp
+++ ceiling.cpp
@@ -1,26 +1,6 @@
 /**
 Adam Rosenberg
-
-
-Advanced Ceiling Manufacturers (ACM) is analyzing the properties of its new series of Incredibly Collapse-Proof Ceilings (ICPCs). An ICPC consists of nn layers of material, each with a different value of collapse resistance (measured as a positive integer). The analysis ACM wants to run will take the collapse-resistance values of the layers, store them in a binary search tree, and check whether the shape of this tree in any way correlates with the quality of the whole construction. Because, well, why should it not?
-
-To be precise, ACM takes the collapse-resistance values for the layers, ordered from the top layer to the bottom layer, and inserts them one-by-one into a tree. The rules for inserting a value vv are:
-
-If the tree is empty, make vv the root of the tree.
-If the tree is not empty, compare vv with the root of the tree. If vv is smaller, insert vv into the left subtree of the root, otherwise insert vv into the right subtree.
-ACM has a set of ceiling prototypes it wants to analyze by trying to collapse them. It wants to take each group of ceiling prototypes that have trees of the same shape and analyze them together.
-
-For example, assume ACM is considering five ceiling prototypes with three layers each, as described by Sample Input 1 and shown in Figure 1. Notice that the first prototype’s top layer has collapse-resistance value 2, the middle layer has value 7, and the bottom layer has value 1. The second prototype has layers with collapse-resistance values of 3, 1, and 4 – and yet these two prototypes induce the same tree shape, so ACM will analyze them together.
-
-Given a set of prototypes, your task is to determine how many different tree shapes they induce.
-
-Input
-The first line of the input contains two integers nn (1≤n≤501≤n≤50), which is the number of ceiling prototypes to analyze, and kk (1≤k≤201≤k≤20), which is the number of layers in each of the prototypes.
-
-The next nn lines describe the ceiling prototypes. Each of these lines contains kk distinct integers (between 11 and 106106, inclusive), which are the collapse-resistance values of the layers in a ceiling prototype, ordered from top to bottom.
-
-Output
-Display the number of different tree shapes.
+Input is a variety of binary search trees, and the output is the number of unique shapes of trees.
 **/
 
 
